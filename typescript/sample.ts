340e1ce9beb8872b7b0bc8ac69fbe68f3b31572c
ikuma <tatsuya.ikuma@rolanddg.co.jp>
2021-01-13 11:32:30 +0900

    test typescript 20201019

---
+++ typescript/sample.ts
@@ -0,0 +1,21 @@
+let a: boolean = true;
+let b = true;
+
+function func(a: number, b: boolean) : number{
+    return a;
+}
+
+// Array
+let aa: string[] = ["a","b"]
+
+// Object
+interface Hoge{
+    a:number,
+    b?:number, //undefined
+    c: number | null, //nullable
+}
+let v: Hoge = {a:10 ,c:null}
+
+
+
+
